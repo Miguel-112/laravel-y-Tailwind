924b1eb22a835018d3d3058bae023f7e73af8239
Miguel-112 <71301712+Miguel-112@users.noreply.github.com>
2022-10-16 21:42:55 -0500

    Se instalo Tailwind
    Se instalo Tailwind

---
+++ tailwind/resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+
+    <h1>Este es un h1</h1>
+    <h2>Este es un h1</h2>
+    <h3>Este es un h1</h2>
+    <h4>Este es un h1</h4>
+    <h5>Este es un h1</h5>
+    <h6>Este es un h1</h6>
+
+    
+</body>
+</html>l
